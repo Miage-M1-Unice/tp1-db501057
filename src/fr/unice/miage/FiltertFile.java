71fe3e8afc4f8c1efdb9390ad5824391e636a071
Benjamin Dekeyser <benjdek@gmail.com>
2019-02-11 16:40:23 +0100

    filter extension

--- src/fr/unice/miage/FiltertFile.java
+++ src/fr/unice/miage/FiltertFile.java
@@ -1,7 +1,16 @@
 package fr.unice.miage;
 
-public interface FiltertFile {
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class FiltertFile implements FilenameFilter {
 
 
-
+    @Override
+    public boolean accept(File dir, String name) {
+        String fileName = dir.getName();
+        String[] splitName = fileName.split("\\.");
+        String fileExtension = splitName[splitName.length - 1];
+		return fileExtension.equals(name);
+    }
 }
--- src/fr/unice/miage/Main.java
+++ src/fr/unice/miage/Main.java
@@ -2,11 +2,10 @@ package fr.unice.miage;
 
 import java.io.File;
 
-public class Main implements FiltertFile {
+public class Main {
 
 
     public void getRepository(){
-        String[] tabRepos;
         File f = new File("./src/fr/unice/miage/");
         String[] flist = f.list();
         for (int i = 0; i < flist.length; i++){
@@ -15,17 +14,17 @@ public class Main implements FiltertFile {
     }
 
     public void getRepoContentRecu(String pathName){
-        String[] tabRepos;
+        FiltertFile filter = new FiltertFile();
         File f = new File(pathName);
         File[] flist = f.listFiles();
         for (int i = 0; i < flist.length; i++){
             if(flist[i].isDirectory()){
-                //System.out.println(flist[i]);
                 getRepoContentRecu(flist[i].getPath());
             } else {
-                System.out.println(flist[i].getName());
+                if (filter.accept(flist[i], "java")) {
+                    System.out.println(flist[i].getName());
+                }
             }
-            //System.out.println(flist[i]);
         }
     }
 
